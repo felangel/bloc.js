89d44057a668f2e842168ff4fcffba02cf2222d6
weiiew <weiwei.li@honeybeehub.ca>
2021-01-30 22:19:48 -0500

    ci fail fix

--- packages/react-bloc/lib/src/hooks/use-bloc-builder.tsx
+++ packages/react-bloc/lib/src/hooks/use-bloc-builder.tsx
@@ -1,46 +1,42 @@
-import React from "react";
-import { Subscription } from "rxjs";
+import React from 'react'
+import { Subscription } from 'rxjs'
 
 interface BlocBuilderOptions {
-  condition: (previous: any, current: any) => boolean;
+  condition: (previous: any, current: any) => boolean
 }
 
-export default function useBlocBuilder(
-  bloc: any,
-  options?: BlocBuilderOptions
-) {
-  const [state, setState] = React.useState(bloc.state);
-  const mounted = React.useRef<any>(false);
-  const previousState = React.useRef<any>(null);
-  const subscription = React.useRef<any>(Subscription.EMPTY);
-  const thisBloc = React.useRef<any>(bloc);
+export default function useBlocBuilder(bloc: any, options?: BlocBuilderOptions) {
+  const [state, setState] = React.useState(bloc.state)
+  const mounted = React.useRef<any>(false)
+  const previousState = React.useRef<any>(null)
+  const subscription = React.useRef<any>(Subscription.EMPTY)
+  const thisBloc = React.useRef<any>(bloc)
 
   const subscribe = () => {
     subscription.current = thisBloc.current.listen((s: any) => {
-      let rebuild: boolean = options?.condition
-        ? options.condition.call(null, state, s)
-        : true;
+      let rebuild: boolean =
+        options && options.condition !== null ? options.condition.call(null, state, s) : true
 
-      if (rebuild) setState(s);
-      previousState.current = state;
-    });
-  };
+      if (rebuild) setState(s)
+      previousState.current = state
+    })
+  }
 
   const unsubscribe = () => {
-    subscription.current.unsubscribe();
-  };
+    subscription.current.unsubscribe()
+  }
 
   React.useLayoutEffect(() => {
     if (!mounted.current) {
-      mounted.current = true;
-      subscribe();
+      mounted.current = true
+      subscribe()
     } else {
-      unsubscribe();
-      thisBloc.current = bloc;
-      subscribe();
+      unsubscribe()
+      thisBloc.current = bloc
+      subscribe()
     }
-    return () => unsubscribe();
-  });
+    return () => unsubscribe()
+  })
 
-  return { state };
+  return { state }
 }

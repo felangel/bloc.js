fec062df9d58b9ef5cfa6170881301bcb4bd9371
Aravind <63452117+codesculpture@users.noreply.github.com>
2023-07-02 11:36:52 +0530

    Added Test and JsDoc(Not perfect)

--- packages/bloc/lib/src/bloc-base.ts
+++ packages/bloc/lib/src/bloc-base.ts
@@ -22,6 +22,13 @@ export class BlocBase<State> extends Observable<State> {
         return this._stateSubject.isStopped;
     }
 
+    /**
+     * Update the given `state` with existing state and emits the new `state` to its
+     * listeners
+     * @param {State} state 
+     * @memberof BlocBase
+     */
+
     emit(state: State): void {
         try {
             if (this.isClosed) {
@@ -40,18 +47,47 @@ export class BlocBase<State> extends Observable<State> {
         }
     }
 
+    /**
+     * Called whenever an error is added while emitting and call onError.
+     *
+     * @param {Error} error
+     * @memberof BlocBase
+     */
     addError(error: any): void {
         this.onError(error);
     }
+
+    /**
+     * Called whenever a `change` occurs with the given `change`.
+     * A `change` occurs when a new `state` is added.
+     * `onChange` is called before a bloc's or cubit's state has been updated.
+     *
+     * @param {State} _currentState existing State that it currently hold
+     * @param {State} _nextState new State which is to be updated
+     * @memberof BlocBase
+     */
 
     onChange(_currentState: State, _nextState: State): void {
         this._blocObserver.onChange(this, _currentState, _nextState)
     }
 
+    /**
+     * Called whenever an error is occured when emitting.
+     *
+     * @param {Error} error
+     * @memberof BlocBase
+     */
     onError(error: any): void {
         this._blocObserver.onError(this, error);
     }
 
+    /**
+     * This method should be called when a `Bloc` or `Cubit` is no longer needed.
+     * Disposes the resources held by the bloc or cubit which means the `Bloc` or `Cubit` will
+     * no longer process new events after `close` has been called.
+     *
+     * @memberof BlocBase
+     */
     close(): void {
         this._stateSubject.complete();
         this._blocObserver.onClose(this);
